0e228acd974d4e451959e8b6f319f977d95409d1
roberto_cuellar <lozano.roberto.2018@gmail.com>
2023-05-27 15:39:23 -0500

    add configuration to tailwindcss custom styles

--- front/front-e-c/tailwind.config.js
+++ front/front-e-c/tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      background: '#0a192f',
+      background_ligth: '#112240',
+      primary: '#ccd6f6',
+      primary_ligth: '#8892b0',
+      secondary: '#64ffda'
+    }
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function({ addUtilities, theme }) {
+      const newUtilities = {
+        '.btn-primary': {
+          'border': `solid 1px ${theme('colors.secondary')}`,
+          'border-radius': '3px',
+          'padding':'10px',
+          'color': theme('colors.secondary')
+        }
+      }
 
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    })
+  ],
+}
